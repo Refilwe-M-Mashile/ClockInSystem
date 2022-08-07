87389007b235259549a7adc93a906e337c3dadc1
Ms-P-Hub <gpm.mokwena@gmail.com>
2022-08-07 07:55:43 +0200

    Worked on reg

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,1 +1,10 @@
 const btnRegister = document.querySelector('#btnRegister');
+
+btnRegister.addEventListener(()=>{
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const email = document.getElementById("email").value;
+    const tel = document.getElementById("tel").value;
+    const rate = document.getElementById("rate").value;
+})
+
